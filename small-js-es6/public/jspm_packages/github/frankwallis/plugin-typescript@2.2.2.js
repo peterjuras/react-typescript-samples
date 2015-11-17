be0a8efe1660330912c3cf8303ea8344ebdc96ea
Peter Juras <p.juras@outlook.com>
2015-11-17 01:23:42 +0100

    Ignore node_modules & jspm_packages

--- small-js-es6/public/jspm_packages/github/frankwallis/plugin-typescript@2.2.2.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("github:frankwallis/plugin-typescript@2.2.2/plugin");
--- small-js-es6/server.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.listen(3000);
